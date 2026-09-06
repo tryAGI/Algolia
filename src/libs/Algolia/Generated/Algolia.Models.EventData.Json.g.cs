f1cb92fff2c3858870e85dba345b17e5f7846d6a
Konstantin S. <havendv@gmail.com>
2026-09-07 02:40:45 +0400

    feat: Updated OpenAPI spec (#79)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Algolia/Generated/Algolia.Models.EventData.Json.g.cs
+++ src/libs/Algolia/Generated/Algolia.Models.EventData.Json.g.cs
@@ -2,7 +2,7 @@
 
 namespace Algolia
 {
-    public sealed partial class EventData2
+    public sealed partial class EventData
     {
         /// <summary>
         /// Serializes the current instance to a JSON string using the provided JsonSerializerContext.
@@ -47,20 +47,20 @@ namespace Algolia
         /// <summary>
         /// Deserializes a JSON string using the provided JsonSerializerContext.
         /// </summary>
-        public static global::Algolia.EventData2? FromJson(
+        public static global::Algolia.EventData? FromJson(
             string json,
             global::System.Text.Json.Serialization.JsonSerializerContext jsonSerializerContext)
         {
             return global::System.Text.Json.JsonSerializer.Deserialize(
                 json,
-                typeof(global::Algolia.EventData2),
-                jsonSerializerContext) as global::Algolia.EventData2;
+                typeof(global::Algolia.EventData),
+                jsonSerializerContext) as global::Algolia.EventData;
         }
 
         /// <summary>
         /// Deserializes a JSON string using the generated default JsonSerializerContext.
         /// </summary>
-        public static global::Algolia.EventData2? FromJson(
+        public static global::Algolia.EventData? FromJson(
             string json)
         {
             return FromJson(
@@ -75,7 +75,7 @@ namespace Algolia
         [global::System.Diagnostics.CodeAnalysis.RequiresUnreferencedCode("JSON serialization and deserialization might require types that cannot be statically analyzed. Use the overload that takes a JsonTypeInfo or JsonSerializerContext, or make sure all of the required types are preserved.")]
         [global::System.Diagnostics.CodeAnalysis.RequiresDynamicCode("JSON serialization and deserialization might require types that cannot be statically analyzed and might need runtime code generation. Use System.Text.Json source generation for native AOT applications.")]
 #endif
-        public static global::Algolia.EventData2? FromJson(
+        public static global::Algolia.EventData? FromJson(
             string json,
             global::System.Text.Json.JsonSerializerOptions? jsonSerializerOptions = null)
         {
@@ -86,7 +86,7 @@ namespace Algolia
                     global::Algolia.SourceGenerationContext.Default);
             }
 
-            return global::System.Text.Json.JsonSerializer.Deserialize<global::Algolia.EventData2>(
+            return global::System.Text.Json.JsonSerializer.Deserialize<global::Algolia.EventData>(
                 json,
                 jsonSerializerOptions);
         }
@@ -94,20 +94,20 @@ namespace Algolia
         /// <summary>
         /// Deserializes a JSON stream using the provided JsonSerializerContext.
         /// </summary>
-        public static async global::System.Threading.Tasks.ValueTask<global::Algolia.EventData2?> FromJsonStreamAsync(
+        public static async global::System.Threading.Tasks.ValueTask<global::Algolia.EventData?> FromJsonStreamAsync(
             global::System.IO.Stream jsonStream,
             global::System.Text.Json.Serialization.JsonSerializerContext jsonSerializerContext)
         {
             return (await global::System.Text.Json.JsonSerializer.DeserializeAsync(
                 jsonStream,
-                typeof(global::Algolia.EventData2),
-                jsonSerializerContext).ConfigureAwait(false)) as global::Algolia.EventData2;
+                typeof(global::Algolia.EventData),
+                jsonSerializerContext).ConfigureAwait(false)) as global::Algolia.EventData;
         }
 
         /// <summary>
         /// Deserializes a JSON stream using the generated default JsonSerializerContext.
         /// </summary>
-        public static global::System.Threading.Tasks.ValueTask<global::Algolia.EventData2?> FromJsonStreamAsync(
+        public static global::System.Threading.Tasks.ValueTask<global::Algolia.EventData?> FromJsonStreamAsync(
             global::System.IO.Stream jsonStream)
         {
             return FromJsonStreamAsync(
@@ -122,7 +122,7 @@ namespace Algolia
         [global::System.Diagnostics.CodeAnalysis.RequiresUnreferencedCode("JSON serialization and deserialization might require types that cannot be statically analyzed. Use the overload that takes a JsonTypeInfo or JsonSerializerContext, or make sure all of the required types are preserved.")]
         [global::System.Diagnostics.CodeAnalysis.RequiresDynamicCode("JSON serialization and deserialization might require types that cannot be statically analyzed and might need runtime code generation. Use System.Text.Json source generation for native AOT applications.")]
 #endif
-        public static global::System.Threading.Tasks.ValueTask<global::Algolia.EventData2?> FromJsonStreamAsync(
+        public static global::System.Threading.Tasks.ValueTask<global::Algolia.EventData?> FromJsonStreamAsync(
             global::System.IO.Stream jsonStream,
             global::System.Text.Json.JsonSerializerOptions? jsonSerializerOptions = null)
         {
@@ -133,7 +133,7 @@ namespace Algolia
                     global::Algolia.SourceGenerationContext.Default);
             }
 
-            return global::System.Text.Json.JsonSerializer.DeserializeAsync<global::Algolia.EventData2?>(
+            return global::System.Text.Json.JsonSerializer.DeserializeAsync<global::Algolia.EventData?>(
                 jsonStream,
                 jsonSerializerOptions);
         }
