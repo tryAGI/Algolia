44f598ff98f0ff7383dff5d4698b10747eb0c3ea
Konstantin S. <havendv@gmail.com>
2026-09-14 00:16:36 +0400

    feat: Updated OpenAPI spec (#82)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

---
+++ src/libs/Algolia/Generated/Recommend/Algolia.Recommend.IRecommendClient.Authorizations.Bearer.g.cs
@@ -0,0 +1,16 @@
+
+#nullable enable
+
+namespace Algolia.Recommend
+{
+    public partial interface IRecommendClient
+    {
+        /// <summary>
+        /// Authorize using bearer authentication.
+        /// </summary>
+        /// <param name="apiKey"></param>
+
+        public void AuthorizeUsingBearer(
+            string apiKey);
+    }
+}
